20373965014595b6570aa4ed57ba8d0ddfe06d1c
Dilbar Isakova <geekwarns@gmail.com>
2022-08-15 14:05:18 +0600

    updates

---
+++ LoactionButton/LoactionButton/ContentView.swift
@@ -0,0 +1,71 @@
+//
+//  ContentView.swift
+//  LoactionButton
+//
+//  Created by eesaack on 2022-06-20.
+//
+
+import CoreLocationUI
+import MapKit
+import SwiftUI
+
+struct ContentView: View {
+    
+    @StateObject private var viewModel = ContentViewModel()
+    
+    var body: some View {
+        ZStack (alignment: .bottom){
+            Map(coordinateRegion: $viewModel.region, showsUserLocation: true)
+                .ignoresSafeArea()
+                .tint(.pink)
+            
+            LocationButton(.currentLocation) {
+                viewModel.requestAllowOnceLocationPermission()
+            }
+            .foregroundColor(.white)
+            .cornerRadius(8)
+            .labelStyle(.titleAndIcon)
+            .symbolVariant(.fill)
+            .tint(.pink)
+            .padding(.bottom, 50)
+            
+        }
+    }
+
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+final class ContentViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+    @Published var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 40, longitude: 120), span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10))
+    
+    let locationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+    }
+    
+    func requestAllowOnceLocationPermission() {
+        locationManager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let latestLocation = locations.first else {
+            //show en error
+            return
+    }
+        
+        DispatchQueue.main.async {
+            self.region = MKCoordinateRegion(center: latestLocation.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
+    }
+
---
+++ LoactionButton/LoactionButton/LoactionButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoactionButtonApp.swift
+//  LoactionButton
+//
+//  Created by eesaack on 2022-06-20.
+//
+
+import SwiftUI
+
+@main
+struct LoactionButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
